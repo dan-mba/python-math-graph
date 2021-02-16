0298ccce02e49229b3d37e0db17c3b188842ec80
Dan <35927536+dan-mba@users.noreply.github.com>
2021-02-16 09:11:25 -0500

    autopep8 formatting

--- math-graph/graph.py
+++ math-graph/graph.py
@@ -10,23 +10,24 @@ output:
 https://technovechno.com/creating-graphs-in-python-using-matplotlib-flask-framework-pythonanywhere/
 """
 
+import numpy as np
+import base64
+import io
+from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-import io
-import base64
-import numpy as np
- 
+
+
 def build_graph(x_coordinates, y_coordinates, split_neg=False):
-  img = io.BytesIO()
-  if split_neg:
-    pos = np.flatnonzero(x_coordinates>0)[0]
-    plt.plot(x_coordinates[:pos],y_coordinates[:pos], color='b')
-    plt.plot(x_coordinates[pos:],y_coordinates[pos:], color='b')
-  else:
-    plt.plot(x_coordinates, y_coordinates, color='b')
-  plt.savefig(img, format='png')
-  img.seek(0)
-  graph_url = base64.b64encode(img.getvalue()).decode()
-  plt.close()
-  return 'data:image/png;base64,{}'.format(graph_url)
+    img = io.BytesIO()
+    if split_neg:
+        pos = np.flatnonzero(x_coordinates > 0)[0]
+        plt.plot(x_coordinates[:pos], y_coordinates[:pos], color='b')
+        plt.plot(x_coordinates[pos:], y_coordinates[pos:], color='b')
+    else:
+        plt.plot(x_coordinates, y_coordinates, color='b')
+    plt.savefig(img, format='png')
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return 'data:image/png;base64,{}'.format(graph_url)
